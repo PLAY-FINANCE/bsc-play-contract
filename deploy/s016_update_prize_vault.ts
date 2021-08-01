6b5aa860f37a18aae4d56b039d3eac50b4dfab53
joo.mingyu <play.finance.dev@gmail.com>
2021-08-01 12:51:39 +0900

    deploy: add update_prize_vault

---
+++ deploy/s016_update_prize_vault.ts
@@ -0,0 +1,34 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+import { PrizeVault__factory } from '../typechain';
+import MainnetConfig from '../.mainnet.json';
+import TestnetConfig from '../.testnet.json';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { network } = hre;
+  const config = network.name === "mainnet" ? MainnetConfig : TestnetConfig;
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  const prizeVault = PrizeVault__factory.connect(config.Vault.Prize0, (await ethers.getSigners())[0]);
+  
+  console.log(">> Set last reward block");
+  const blockNumber = await ethers.provider.getBlockNumber();
+  (await prizeVault.setLastRewardBlock(blockNumber, { gasLimit: '1000000' })).wait();
+  console.log("✅ Done");
+
+  console.log(">> Set prize per blcok");
+  (await prizeVault.setPrizePerBlock(ethers.utils.parseEther('0.001'), { gasLimit: '1000000' })).wait();
+  console.log("✅ Done");
+};
+
+export default func;
+func.tags = ["s016"];
