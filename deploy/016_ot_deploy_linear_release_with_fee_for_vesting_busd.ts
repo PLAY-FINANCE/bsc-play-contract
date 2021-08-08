b17839d2f27a2e53dbac5e3098930270c09951ea
joo.mingyu <play.finance.dev@gmail.com>
2021-08-08 18:44:49 +0900

    deploy: add LinearReleaseWithFee

---
+++ deploy/016_ot_deploy_linear_release_with_fee_for_vesting_busd.ts
@@ -0,0 +1,53 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+import MainnetConfig from '../.mainnet.json';
+import TestnetConfig from '../.testnet.json';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const config = network.name === "mainnet" ? MainnetConfig : TestnetConfig;
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  console.log(">> Deploying a Linear Release With Fee contract");
+  await deploy('LinearReleaseWithFeeBusd', {
+    from: deployer,
+    args: [
+      config.Tokens.BUSD,
+      config.Tokens.PLAY,
+      ethers.utils.parseEther('1'),
+      config.Funds.Operator
+    ],
+    contract: 'LinearReleaseWithFee',
+    log: true,
+    deterministicDeployment: false,
+  });
+  console.log("✅ Done");
+};
+
+export default func;
+func.tags = ['LinearReleaseWithFeeBusd'];
